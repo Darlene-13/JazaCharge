2fee120ab96445a62049373793c33346d21cf84f
Darlene-13 <darlenewendie@gmail.com>
2026-06-24 09:25:21 +0300

    Initialize project: User entity

---
+++ src/main/java/io/github/darlene/jazacharge/entity/User.java
@@ -0,0 +1,36 @@
+package io.github.darlene.jazacharge.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String userName;
+
+    // To be hashed using Argon2
+    @Column(name = "password", nullable = false, unique = true)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
---
+++ src/test/java/io/github/darlene/jazacharge/JazaChargeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.darlene.jazacharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazaChargeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
